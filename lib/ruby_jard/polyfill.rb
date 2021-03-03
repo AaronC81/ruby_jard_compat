c25f141361903c16e158623dcb6a62ddb49d3355
Aaron Christiansen <aaronc20000@gmail.com>
2021-03-03 00:21:07 +0000

    Fix &. polyfill, add Thread#name polyfill

--- lib/ruby_jard/polyfill.rb
+++ lib/ruby_jard/polyfill.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class Object
-  # This is just Tryable#try! from ActiveSupport
+  # This is just Tryable#try! from ActiveSupport, but with the return added
   def jard_nilsafe(method_name = nil, *args, &b)
+    return nil if nil?
     if method_name.nil? && block_given?
       if b.arity == 0
         instance_eval(&b)
@@ -13,4 +14,8 @@ class Object
       public_send(method_name, *args, &b)
     end
   end
+end
+
+class Thread
+  attr_accessor :name unless Thread.method_defined?(:name)
 end
